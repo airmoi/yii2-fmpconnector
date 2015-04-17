3483c3022e78fe1630d3f32c5d11dc620397f568
Romain Dunand <romain@dunand.me>
2015-04-17 20:55:59 +0200

    Added custom gii crud generator

---
+++ gii/templates/crud/Generator.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+
+namespace airmoi\yii2fmconnector\gii\templates\crud;
+
+use Yii;
+use yii\db\ActiveRecord;
+use yii\db\BaseActiveRecord;
+use yii\db\Schema;
+use yii\gii\CodeFile;
+use yii\helpers\Inflector;
+use yii\helpers\VarDumper;
+use yii\web\Controller;
+
+/**
+ * Generates CRUD
+ *
+ * @property array $columnNames Model column names. This property is read-only.
+ * @property string $controllerID The controller ID (without the module ID prefix). This property is
+ * read-only.
+ * @property array $searchAttributes Searchable attributes. This property is read-only.
+ * @property boolean|\yii\db\TableSchema $tableSchema This property is read-only.
+ * @property string $viewPath The controller view path. This property is read-only.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class Generator extends \yii\gii\generators\crud\Generator
+{
+    /**
+     * Generates code for active field
+     * @param string $attribute
+     * @return string
+     */
+    public function generateActiveField($attribute)
+    {
+        $tableSchema = $this->getTableSchema();
+        if ($tableSchema === false || !isset($tableSchema->columns[$attribute])) {
+            if (preg_match('/^(password|pass|passwd|passcode)$/i', $attribute)) {
+                return "\$form->field(\$model, '$attribute')->passwordInput()";
+            } else {
+                return "\$form->field(\$model, '$attribute')";
+            }
+        }
+        $column = $tableSchema->columns[$attribute];
+        if ($column->phpType === 'boolean') {
+            return "\$form->field(\$model, '$attribute')->checkbox()";
+        } elseif ($column->type === 'text') {
+            return "\$form->field(\$model, '$attribute')->textarea(['rows' => 6])";
+        } else {
+            if (preg_match('/^(password|pass|passwd|passcode)$/i', $column->name)) {
+                $input = 'passwordInput';
+            } else {
+                $input = 'textInput';
+            }
+            /* Search if column is a foreign key and generate dropDownList*/
+            foreach ( $tableSchema->foreignKeys as $relation) {
+                if ( array_key_exists($attribute, $relation)) {
+                    return "\$form->field(\$model, '$attribute')->dropDownList("
+                    . $relation[0]."::valueList()" .", ['prompt' => ''])";
+                }
+            }
+            
+            if ($column->phpType !== 'string' || $column->size === null) {
+                return "\$form->field(\$model, '$attribute')->$input()";
+            } else {
+                return "\$form->field(\$model, '$attribute')->$input(['maxlength' => $column->size])";
+            }
+        }
+    }
+}
