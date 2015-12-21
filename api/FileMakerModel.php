fd76ef91937b9294ae284d92c633efc589b3f389
Romain Dunand <airmoi@gmail.com>
2015-12-21 11:51:49 +0100

    Incorrect "no record found" error code trapping

--- api/FileMakerModel.php
+++ api/FileMakerModel.php
@@ -100,7 +100,7 @@ class FileMakerModel extends \yii\db\BaseActiveRecord
             return $array;
         } catch( \airmoi\FileMaker\FileMakerException $ex){
             // catch "not found" error
-            if ( $ex->getCode() == '101'){
+            if ( $ex->getCode() == '401'){
                 return [];
             }
             throw $ex;
